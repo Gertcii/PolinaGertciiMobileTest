62556b0aea4903742c36107852ac7f7cba7d0d42
Gertsii <polishkagerc@gmail.com>
2021-02-12 14:14:19 +0300

    ex-2 commit 1.0

--- src/main/java/pageObjects/NativePageObject.java
+++ src/main/java/pageObjects/NativePageObject.java
@@ -11,6 +11,34 @@ public class NativePageObject  {
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
     WebElement signInBtn;
 
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
+    WebElement regAccount;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
+    WebElement emailField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
+    WebElement userNameField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
+    WebElement passField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
+    WebElement confPassField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
+    WebElement regNewAccount;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
+    WebElement loginField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
+    WebElement passwordField;
+
+    @AndroidFindBy(xpath = "//*[contains(@text, 'BudgetActivity')]")
+    WebElement textView;
+
+
     public NativePageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements( new AppiumFieldDecorator(appiumDriver), this);
     }
